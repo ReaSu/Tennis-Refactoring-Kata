28d51d97daf9c7fbe0b34bf9175194520ca7581f
Regula Sutter <regula.sutter@univie.ac.at>
2021-04-13 17:33:17 +0200

    Kommentar zu Testcases

--- java/src/test/java/tennis/game/TennisTest.java
+++ java/src/test/java/tennis/game/TennisTest.java
@@ -1,14 +1,14 @@
 package tennis.game;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class TennisTest {
@@ -22,58 +22,76 @@ public class TennisTest {
         this.player2Score = player2Score;
         this.expectedScore = expectedScore;
     }
-    
+
     @Parameters
     public static Collection<Object[]> getAllScores() {
-        return Arrays.asList(new Object[][] {
-                { 0, 0, "Love-All" },
-                { 1, 1, "Fifteen-All" },
-                { 2, 2, "Thirty-All"},
-                { 3, 3, "Deuce"},
-                { 4, 4, "Deuce"},
-                
-                { 1, 0, "Fifteen-Love"},
-                { 0, 1, "Love-Fifteen"},
-                { 2, 0, "Thirty-Love"},
-                { 0, 2, "Love-Thirty"},
-                { 3, 0, "Forty-Love"},
-                { 0, 3, "Love-Forty"},
-                { 4, 0, "Win for player1"},
-                { 0, 4, "Win for player2"},
-                
-                { 2, 1, "Thirty-Fifteen"},
-                { 1, 2, "Fifteen-Thirty"},
-                { 3, 1, "Forty-Fifteen"},
-                { 1, 3, "Fifteen-Forty"},
-                { 4, 1, "Win for player1"},
-                { 1, 4, "Win for player2"},
+        return Arrays.asList(new Object[][]{
+                /*
+                 * (2)
+                 * TESTS fachlich prüfen
+                 * Eingabe Wertebereich [0-99 x 0-99]
+                 * Ausgabe Werte nach Liste
+                 * ? Diagramm aller Zustände
+                 * https://dzone.com/articles/scoring-tennis-using-finite
+                 * Tests sind fachlich vollständig!
+                 */
 
-                { 3, 2, "Forty-Thirty"},
-                { 2, 3, "Thirty-Forty"},
-                { 4, 2, "Win for player1"},
-                { 2, 4, "Win for player2"},
-                
-                { 4, 3, "Advantage player1"},
-                { 3, 4, "Advantage player2"},
-                { 5, 4, "Advantage player1"},
-                { 4, 5, "Advantage player2"},
-                { 15, 14, "Advantage player1"},
-                { 14, 15, "Advantage player2"},
+                // gleiche zahlen
+                {0, 0, "Love-All"},
+                {1, 1, "Fifteen-All"},
+                {2, 2, "Thirty-All"},
+                {3, 3, "Deuce"},
+                {4, 4, "Deuce"},
 
-                { 6, 4, "Win for player1"},
-                { 4, 6, "Win for player2"},
-                { 16, 14, "Win for player1"},
-                { 14, 16, "Win for player2"},
+                // mit 0
+                {1, 0, "Fifteen-Love"},
+                {0, 1, "Love-Fifteen"},
+                {2, 0, "Thirty-Love"},
+                {0, 2, "Love-Thirty"},
+                {3, 0, "Forty-Love"},
+                {0, 3, "Love-Forty"},
+                {4, 0, "Win for player1"},
+                {0, 4, "Win for player2"},
+
+                // mit 1
+                {2, 1, "Thirty-Fifteen"},
+                {1, 2, "Fifteen-Thirty"},
+                {3, 1, "Forty-Fifteen"},
+                {1, 3, "Fifteen-Forty"},
+                {4, 1, "Win for player1"},
+                {1, 4, "Win for player2"},
+
+                // mit 2
+                {3, 2, "Forty-Thirty"},
+                {2, 3, "Thirty-Forty"},
+                {4, 2, "Win for player1"},
+                {2, 4, "Win for player2"},
+
+                // mit 1 Differenz
+                {4, 3, "Advantage player1"},
+                {3, 4, "Advantage player2"},
+                {5, 4, "Advantage player1"},
+                {4, 5, "Advantage player2"},
+                {15, 14, "Advantage player1"},
+                {14, 15, "Advantage player2"},
+
+                // mit 2 Differenz
+                {6, 4, "Win for player1"},
+                {4, 6, "Win for player2"},
+                {16, 14, "Win for player1"},
+                {14, 16, "Win for player2"},
         });
     }
 
-    public void checkAllScores(TennisGame game) {
+    private void checkAllScores(TennisGame game) {
         int highestScore = Math.max(this.player1Score, this.player2Score);
         for (int i = 0; i < highestScore; i++) {
-            if (i < this.player1Score)
+            if (i < this.player1Score) {
                 game.wonPoint("player1");
-            if (i < this.player2Score)
+            }
+            if (i < this.player2Score) {
                 game.wonPoint("player2");
+            }
         }
         assertEquals(this.expectedScore, game.getScore());
     }
@@ -84,3 +102,11 @@ public class TennisTest {
         checkAllScores(game);
     }
 }
+
+/*
+ * (1)
+ * Analyse des IN/OUT
+ * IN: Constructor mit player1 name und player2 name
+ * IN: game.wonPoint(player name) ... Spieler scored 1 point
+ * OUT: game.getScore() ...Score Spielstand als Text
+ */
